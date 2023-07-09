aa4638553cda9fe1b3ff93ce448c784cdf05743e
PettyDriedFish <lizexuan0407@163.com>
2023-07-09 14:22:41 -0700

    add send temp and hum every 60 seconds

--- code/websocketServer.py
+++ code/websocketServer.py
@@ -2,11 +2,12 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import getData
+import time
 from kasa import SmartPlug
 import asyncio
 
-temp = getData.getCurrentTemp()
-hum = getData.getCurrentHum()
+# temp = getData.getCurrentTemp()
+# hum = getData.getCurrentHum()
 
 # fanPlug = asyncio.run(SmartPlug("192.168.3.29").is_on)
 # humidifierPlug = asyncio.run(SmartPlug("192.168.3.30").is_on)
@@ -15,13 +16,15 @@ hum = getData.getCurrentHum()
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print("WebSocket connection opened")
+        self.send_temperature_humidity()  # 初始连接时立即发送一次温湿度消息
 
     def on_message(self, message):
         print("Received message:", message)
+        self.send_temperature_humidity()  # 收到任何消息时都发送一次温湿度消息
         if message == '1':
-            writeMessage = f'Temperature now is {temp}'
+            writeMessage = f'Temperature now is temp'
         elif message == '2':
-            writeMessage = f'Humidity now is {hum}'
+            writeMessage = f'Humidity now is hum'
         # elif message == '3':
         #     writeMessage = f'fanPlug now is {fanPlug}'
         # elif message == '4':
@@ -34,6 +37,18 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     def on_close(self):
         print("WebSocket connection closed")
+
+    def send_temperature_humidity(self):
+        while True:
+            temp = getData.getCurrentTemp()
+            hum = getData.getCurrentHum()
+
+            writeMessage = f'Temperature now is {temp}, Humidity now is {hum}'
+            print(writeMessage)
+            self.write_message(writeMessage)
+
+            # wait for 60 seconds
+            time.sleep(60)
 
 if __name__ == "__main__":
     app = tornado.web.Application([
