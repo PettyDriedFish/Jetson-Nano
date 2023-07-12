e612871e127585eb3d0f51baf552b1836aad9d19
PettyDriedFish <lizexuan0407@163.com>
2023-07-12 14:12:13 -0700

    communication with set plugs status, phone to jetson

--- code/websocketServer.py
+++ code/websocketServer.py
@@ -1,3 +1,4 @@
+import kasa
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
@@ -10,18 +11,10 @@ import asyncio
 import tracemalloc
 tracemalloc.start()
 
-# temp = getData.getCurrentTemp()
-# hum = getData.getCurrentHum()
-
-# fan_plug_status = asyncio.run(SmartPlug("192.168.3.29").is_on)
-# humidifier_plug_status = asyncio.run(SmartPlug("192.168.3.30").is_on)
-# heater_plug_status = asyncio.run(SmartPlug("192.168.3.31").is_on)
-
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print("WebSocket connection opened")
         tornado.ioloop.IOLoop.current().spawn_callback(self.send_data)
-        # self.send_temperature_humidity()
 
     def on_message(self, message):
         print("Received message:", message)
@@ -30,64 +23,31 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         humidifier_plug_status = data['humidifier_plug_status']
         heater_plug_status = data['heater_plug_status']
 
-        # self.send_data()  # 收到任何消息时都发送一次消息
-        # if message == '1':
-        #     writeMessage = f'Temperature now is temp'
-        # elif message == '2':
-        #     writeMessage = f'Humidity now is hum'
-        # # elif message == '3':
-        # #     writeMessage = f'fanPlug now is {fanPlug}'
-        # # elif message == '4':
-        # #     writeMessage = f'humidifierPlug now is {humidifierPlug}'
-        # # elif message == '5':
-        # #     writeMessage = f'heaterPlug now is {heaterPlug}'
-        # else:
-        #     writeMessage = message
-        # self.write_message(writeMessage)
+        asyncio.create_task(self.control_plug("192.168.3.29", fan_plug_status))
+        asyncio.create_task(self.control_plug("192.168.3.30", humidifier_plug_status))
+        asyncio.create_task(self.control_plug("192.168.3.31", heater_plug_status))
 
     @gen.coroutine
     def on_close(self):
         print("WebSocket connection closed")
         self.close(None)  # 添加这行代码来关闭流对象
 
-    def send_temperature_humidity(self):
-        while True:
-            temp = getData.getCurrentTemp()
-            hum = getData.getCurrentHum()
-
-            writeMessage = f'Temperature now is {temp}, Humidity now is {hum}'
-            print(writeMessage)
-            self.write_message(writeMessage)
-
-            # wait for 60 seconds
-            time.sleep(60)
-
     async def get_smartplug_state(self, ip_address):
-        plug = SmartPlug(ip_address)
-        await plug.update()
-        return plug.is_on
+        try:
+            plug = SmartPlug(ip_address)
+            await plug.update()
+            return plug.is_on
+        except kasa.exceptions.SmartDeviceException:
+            return False  # 假设设备状态为关闭
 
     async def send_data(self):
-        while True:
+        while self.ws_connection:
             temp = getData.getCurrentTemp()
             hum = getData.getCurrentHum()
-
-            # fanPlug = asyncio.run(SmartPlug("192.168.3.29").update())
-            # humidifierPlug = asyncio.run(SmartPlug("192.168.3.30").update())
-            # heaterPlug = asyncio.run(SmartPlug("192.168.3.31").update())
-            #
-            # fan_plug_state = fanPlug.is_on
-            # humidifier_plug_state = humidifierPlug.is_on
-            # heater_plug_state = heaterPlug.is_on
 
             fan_plug_state = await self.get_smartplug_state("192.168.3.29")
             humidifier_plug_state = await self.get_smartplug_state("192.168.3.30")
             heater_plug_state = await self.get_smartplug_state("192.168.3.31")
-
-            # message = f'Temperature now is {temp}, Humidity now is {hum}\n'
-            # message += f'Fan Plug is {"on" if fan_plug_state else "off"}\n'
-            # message += f'Humidifier Plug is {"on" if humidifier_plug_state else "off"}\n'
-            # message += f'Heater Plug is {"on" if heater_plug_state else "off"}'
 
             data = {
                 'temperature': temp,
@@ -100,19 +60,20 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
             message = json.dumps(data)
 
             print(message)
-            await self.write_message(message)
+            if self.ws_connection:
+                await self.write_message(message)
 
             # 等待60秒后再次发送数据
-            time.sleep(10)
+            await gen.sleep(10)
 
-    async def control_plug(ip_address, status):
+    async def control_plug(self, ip_address, plug_status):
         plug = SmartPlug(ip_address)
         await plug.update()
 
-        if status and not plug.is_on:
+        if plug_status and not plug.is_on:
             await plug.turn_on()
             print(f"Plug at {ip_address} turned on")
-        elif not status and plug.is_on:
+        elif not plug_status and plug.is_on:
             await plug.turn_off()
             print(f"Plug at {ip_address} turned off")
 
